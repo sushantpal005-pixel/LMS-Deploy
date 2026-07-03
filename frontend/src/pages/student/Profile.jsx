30e5fc690aa03b84b9a873e19ab56c68ba71d8df
sushantpal005-pixel <sushantpal005@gmail.com>
2026-07-03 18:02:17 +0530

    remove bugs

--- frontend/src/pages/student/Profile.jsx
+++ frontend/src/pages/student/Profile.jsx
@@ -64,19 +64,19 @@ const Profile = () => {
                     <div className='mb-2'>
                         <h1 className='font-semibold text-gray-900 dark:text-white'>
                             Name:
-                            <span className='font-normal text-gray-700 dark:text-gray-300 ml-2'>{user.name}</span>
+                            <span className='font-normal text-gray-700 dark:text-gray-300 ml-2'>{user?.name}</span>
                         </h1>
                     </div>
                     <div className='mb-2'>
                         <h1 className='font-semibold text-gray-900 dark:text-white'>
                             Email:
-                            <span className='font-normal text-gray-700 dark:text-gray-300 ml-2'>{user.email}</span>
+                            <span className='font-normal text-gray-700 dark:text-gray-300 ml-2'>{user?.email}</span>
                         </h1>
                     </div>
                     <div className='mb-2'>
                         <h1 className='font-semibold text-gray-900 dark:text-white'>
                             Role:
-                            <span className='font-normal text-gray-700 dark:text-gray-300 ml-2'>{user.role.toUpperCase()}</span>
+                            <span className='font-normal text-gray-700 dark:text-gray-300 ml-2'>{user?.role?.toUpperCase()}</span>
                         </h1>
                     </div>
                     <Dialog>
@@ -117,8 +117,8 @@ const Profile = () => {
                 <h1 className='font-medium text-lg'>Courses you're enrolled in</h1>
                 <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-5'>
                     {
-                        isLoading ? ([1, 2, 3].map((_, index) => <EnrolledCourseSkeleton key={index} />)) : (user.enrolledCourses.length === 0 ? <h1>You haven't enrolled yet</h1> : (
-                            user.enrolledCourses.map((course) => <Course course={course} key={course._id} />)
+                        isLoading ? ([1, 2, 3].map((_, index) => <EnrolledCourseSkeleton key={index} />)) : (user?.enrolledCourses?.length === 0 ? <h1>You haven't enrolled yet</h1> : (
+                            user?.enrolledCourses?.map((course) => <Course course={course} key={course._id} />)
                         ))
                     }
                 </div>
